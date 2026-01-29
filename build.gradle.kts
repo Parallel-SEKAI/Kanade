3e035d64c128ea3ca9874d0a8be22139d97ae58d
xiaocaoooo <904228000@qq.com>
2026-01-29 10:30:21 +0800

    build: setup pre-commit.ci with unified .editorconfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,16 +13,6 @@ subprojects {
             target("**/*.kt")
             targetExclude("**/build/**")
             ktlint("1.5.0")
-                .editorConfigOverride(
-                    mapOf(
-                        "ktlint_standard_package-name" to "disabled",
-                        "ktlint_standard_function-naming" to "disabled",
-                        "ktlint_standard_no-wildcard-imports" to "disabled",
-                        "ktlint_standard_property-naming" to "disabled",
-                        "ktlint_standard_value-parameter-comment" to "disabled",
-                        "ktlint_standard_backing-property-naming" to "disabled"
-                    )
-                )
         }
         kotlinGradle {
             target("**/*.gradle.kts")
